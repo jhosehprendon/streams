57205470e2302fd6c13eb543f5013797d3a18ebd
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2018-12-18 16:33:38 -0500

    Scaffolding components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Route />
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App;
