693cb95179349e07d25b6e3e52668b26a1e8a1d8
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2018-12-18 16:56:12 -0500

    Hooking up Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from '../components/streams/StreamCreate'
+import StreamEdit from '../components/streams/StreamEdit'
+import StreamDelete from '../components/streams/StreamDelete'
+import StreamShow from '../components/streams/StreamShow'
+import StreamList from '../components/streams/StreamList'
+import Header from './Header'
+
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
-                <Route />
+                <div>
+                    <Header /> 
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
+                </div>
             </BrowserRouter>
         </div>
     )
