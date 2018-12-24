7e347f16b8689e7dbc33e5c81be0cde36a1aa8f9
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2018-12-24 12:57:34 -0500

    Added Modal component using portals

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
     return (
         <div>
-            Delete
+            Stream Delete
+            <Modal />
         </div>
     )
 }
